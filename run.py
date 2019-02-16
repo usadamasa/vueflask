686f0588b0276f44a44040c66b421d95562b94d1
UCHIDA Masaru <usadamasa@gmail.com>
2019-02-17 08:02:44 +0900

    call flask api from vuejs

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from random import *
 
 
 app = Flask(__name__,
             static_folder = "./frontend/dist/static",
             template_folder = "./frontend/dist")
+
+
+@app.route('/api/random')
+def random_number():
+    response = {
+        'randomNumber': randint(1, 100)
+    }
+    return jsonify(response)
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
     return render_template("index.html")
+
